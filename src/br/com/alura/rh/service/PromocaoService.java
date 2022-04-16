9993246cfbb18d14f0c40d53a570db913a0c419b
Henrique Lima <henrique.lima@valecard.com.br>
2022-04-16 10:58:37 -0300

    Herança indesejada parte 2

--- src/br/com/alura/rh/service/PromocaoService.java
+++ src/br/com/alura/rh/service/PromocaoService.java
@@ -1,4 +1,22 @@
 package br.com.alura.rh.service;
 
+import br.com.alura.rh.ValidacaoException;
+import br.com.alura.rh.model.Cargo;
+import br.com.alura.rh.model.Funcionario;
+
 public class PromocaoService {
+    
+    public void promoverFuncionario(Funcionario funcionario, Boolean metaBatida) {
+
+        if(funcionario.getCargo().equals(Cargo.GERENTE)) {
+            throws new ValidacaoException("Gerentes não podem ser promovidos!!");
+        }
+
+        if(metaBatida) {
+            Cargo novoCargo = funcionario.getCargo().getProximoCargo();
+            funcionario.promover(novoCargo);
+        } else {
+            throws new ValidacaoException("Funcionário não bateu a meta!!"); 
+        }
+    }
 }
